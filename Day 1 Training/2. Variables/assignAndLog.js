399c568f25b5393ae9d862624cf6b81914ef936b
harsh-jagtap-josh <harsh.jagtap@joshsoftware.com>
2025-02-13 14:49:02 +0530

    add minor changes- add answer comments in question1

--- Day 1 Training/2. Variables/assignAndLog.js
+++ Day 1 Training/2. Variables/assignAndLog.js
@@ -12,13 +12,13 @@ console.log(
 // Logging var before and after initialization using `const`
 console.log(a1);
 const a1 = 3.14;
-console.log(`var creates a global or function scope variable, the new variable is hoisted and initialized with undefined value, 
-    hence if we try to access the value before initialization it doesn't return an error, it returns an undefined value.`);
+console.log(
+  `works same as let just the difference being the value stored in this variable cannot be changed.`
+);
 
 // Logging var before and after initialization using `var`
 console.log(a2);
 var a2;
 a2 = 3.14;
-console.log(
-  `works same as let just the difference being the value stored in this variable cannot be changed.`
-);
+console.log(`var creates a global or function scope variable, the new variable is hoisted and initialized with undefined value, 
+  hence if we try to access the value before initialization it doesn't return an error, it returns an undefined value.`);
